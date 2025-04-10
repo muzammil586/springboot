8e563f299fc7f697bec30f83992c26b17b818c86
muzammil586 <muhammadmuzammilkhan682.in>
2025-04-10 15:27:05 +0500

    new file

---
+++ demo/src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Student {
+
+	public int add(int a,int b) {
+		
+		return a+b;
+	}
+public int sub(int a,int b) {
+		
+		return a-b;
+	}
+public int mult(int a,int b) {
+	
+	return a-b;
+}
+}
